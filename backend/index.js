798a93101e261c5fd92daf1467c8e2389a890cab
praful247 <spraful339@gmail.com>
2025-08-31 19:59:09 +0530

    users

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     // Catch-all handler: send back React's index.html file for any non-API routes
-    app.get("*", (req, res) => {
+    app.get('/*splat', (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
