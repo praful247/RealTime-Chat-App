af1301ef55377ccb120e2a6515e96ae1840fd78b
praful247 <spraful339@gmail.com>
2025-09-02 22:18:13 +0530

    deploy

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin:process.env.NODE_ENV === "production" ?[process.env.FRONTEND_URL || "http://https://realtime-chat-app-vqmj.onrender.com/:5173"] : ["http://localhost:5173"],
+		origin:process.env.NODE_ENV === "production" ? [process.env.FRONTEND_URL || "https://realtime-chat-app-vqmj.onrender.com"] : ["http://localhost:5173"],
 		methods: ["GET", "POST"],
 	},
 });
