3d530cd705620a5ba7eb0f4f9ea4c8d290e89f1a
praful247 <spraful339@gmail.com>
2025-09-05 19:30:07 +0530

    trying

--- backend/index.js
+++ backend/index.js
@@ -49,9 +49,10 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     // Catch-all handler: send back React's index.html file for any non-API routes
-    app.get('/(.*)', (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
+    
 }
 
 
